7c2b1ca3c0e13a2fe05c8d3e77aa7919839cd67a
Lorenzo Rivosecchi <lorenzo.rivosecchi@botshelf.ai>
2024-02-02 15:35:54 +0100

    add dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,12 @@
-import MaxAPI from "max-api";
 import { Ableton } from "./ableton";
 import { Logger } from "./logger";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const logger = new Logger();
 logger.info("Hello from Node");
 
 const ableton = new Ableton({
   logger,
-});
-
-MaxAPI.addHandlers({
-  async test() {
-    const result = await ableton.sendReceive("/live/test");
-    MaxAPI.outlet(result);
-  },
-  async get_live_version() {
-    const result = await ableton.sendReceive("/live/application/get/version");
-    MaxAPI.outlet(result);
-  },
-  async get_tempo() {
-    const result = await ableton.sendReceive("/live/song/get/tempo");
-    MaxAPI.outlet(result);
-  },
 });
