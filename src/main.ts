e69500406be2b8cb7158a5cf92fcf8bbed2c7aeb
Lorenzo Rivosecchi <lorenzo.rivosecchi@botshelf.ai>
2024-02-03 22:33:42 +0100

    add langchain implementation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import dotenv from "dotenv";
 import { Logger } from "./logger";
 import MaxAPI from "max-api";
-import { handler } from "./openai/handler";
 
 dotenv.config();
 
 export const logger = new Logger();
 logger.info("Hello from Node");
 
-MaxAPI.addHandler("text", (...args) => {
-  handler(args.join(" "));
+MaxAPI.addHandler("text", async (...args) => {
+  const message = args.join(" ");
+  logger.info(`Received message: ${message}`);
+  const { handler } = await import("./langchain/chat");
+  const response = await handler(message);
+  logger.info(`Response: ${response}`);
 });
