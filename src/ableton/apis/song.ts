880dec5453383affc3f67f9f4d9737ab003f594a
Lorenzo Rivosecchi <lorenzo.rivosecchi@botshelf.ai>
2024-02-05 15:11:55 +0100

    simplify

--- src/ableton/apis/song.ts
+++ src/ableton/apis/song.ts
@@ -4,54 +4,7 @@ import { AbletonBus } from "../bus";
 export class SongAPI {
   constructor(private bus: AbletonBus) {}
 
-  /** Start session playback */
-  async startPlaying() {
-    return this.bus.sendAndReturn("/live/song/start_playing");
+  async getTempo() {
+    return await this.bus.sendAndReturn("/live/song/get/tempo");
   }
-  static StartPlaying: FunctionDefinition = {
-    name: "song__start_playing",
-    description: "Start session playback",
-  };
-
-  /** Stop session playback */
-  async stopPlaying() {
-    return this.bus.sendAndReturn("/live/song/stop_playing");
-  }
-  static StopPlaying: FunctionDefinition = {
-    name: "song__stop_playing",
-    description: "Stop session playback",
-  };
-
-  /** Set the current song tempo */
-  async setTempo(tempo: number) {
-    return this.bus.sendAndReturn("/live/song/tempo", [tempo]);
-  }
-  static SetTempo: FunctionDefinition = {
-    name: "song__set_tempo",
-    description: "Set the current song tempo",
-    parameters: {
-      tempo: {
-        type: "number",
-        description: "The tempo to set",
-      },
-    },
-  };
-
-  /** Jump to next cue point */
-  async nextCue() {
-    return this.bus.sendAndReturn("/live/song/next_cue");
-  }
-  static NextCue: FunctionDefinition = {
-    name: "song__next_cue",
-    description: "Jump to next cue point",
-  };
-
-  /** Jump to previous cue point */
-  async prevCue() {
-    return this.bus.sendAndReturn("/live/song/prev_cue");
-  }
-  static PrevCue: FunctionDefinition = {
-    name: "song__prev_cue",
-    description: "Jump to previous cue point",
-  };
 }
