1236746957d6c078027d4287d706e57dd20d3038
Lorenzo Rivosecchi <lorenzo.rivosecchi@botshelf.ai>
2024-02-05 15:12:07 +0100

    correct

--- src/langchain/tools.ts
+++ src/langchain/tools.ts
@@ -2,21 +2,15 @@ import { controller } from "@/ableton/controller";
 import { logger } from "@/logger";
 import { DynamicTool } from "langchain/tools";
 
-const setTempoTool = new DynamicTool({
-  name: "setTempo",
-  description: "Set the BPM of the song",
-  func: async (input: string) => {
-    console.log("input", input);
-    const bpm = parseInt(input);
-    logger.info(`Setting tempo to ${bpm} BPM`);
-    try {
-      await controller.song.setTempo(bpm);
-      return `Tempo set to ${input} BPM`;
-    } catch (e) {
-      logger.error("Error setting tempo", JSON.stringify(e));
-      return `Error setting tempo to ${input} BPM`;
-    }
+const getTempoTool = new DynamicTool({
+  name: "getTempo",
+  description: "Calls the Song API to get the current bpm",
+  async func() {
+    logger.info("Calling API to get curent tempo");
+    const tempo = await controller.song.getTempo();
+    logger.info("Tempo is", tempo);
+    return tempo;
   },
 });
 
-export const tools = [setTempoTool];
+export const tools = [getTempoTool];
