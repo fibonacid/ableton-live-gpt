40ff46fd87e4ffd19b21f7e29b1742763525f048
Lorenzo Rivosecchi <lorenzo.rivosecchi@botshelf.ai>
2024-02-05 15:41:14 +0100

    fix weird loggin

--- src/logger.ts
+++ src/logger.ts
@@ -1,22 +1,17 @@
 import MaxAPI from "max-api";
-import { stringify } from "querystring";
-import { string } from "zod";
 
 export class Logger {
-  private stringify(args: any[]) {
-    return args.map((a) => JSON.stringify(a));
-  }
   info(...args: any[]) {
     console.info(...args);
-    MaxAPI.post(...this.stringify(args), MaxAPI.POST_LEVELS.INFO);
+    MaxAPI.post(...args, MaxAPI.POST_LEVELS.INFO);
   }
   error(...args: any[]) {
     console.error(...args);
-    MaxAPI.post(...this.stringify(args), MaxAPI.POST_LEVELS.ERROR);
+    MaxAPI.post(...args, MaxAPI.POST_LEVELS.ERROR);
   }
   warn(...args: any[]) {
     console.warn(...args);
-    MaxAPI.post(...this.stringify(args), MaxAPI.POST_LEVELS.WARN);
+    MaxAPI.post(...args, MaxAPI.POST_LEVELS.WARN);
   }
 }
 
