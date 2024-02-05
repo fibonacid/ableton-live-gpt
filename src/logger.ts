f0918c5176442b7226776b68f2d9cc2729f64f59
Lorenzo Rivosecchi <lorenzo.rivosecchi@botshelf.ai>
2024-02-05 15:11:42 +0100

    finx printing

--- src/logger.ts
+++ src/logger.ts
@@ -4,19 +4,19 @@ import { string } from "zod";
 
 export class Logger {
   private stringify(args: any[]) {
-    return args.map((a) => JSON.stringify(a)).join(" ");
+    return args.map((a) => JSON.stringify(a));
   }
   info(...args: any[]) {
-    console.info(args);
-    MaxAPI.post(this.stringify(args), MaxAPI.POST_LEVELS.INFO);
+    console.info(...args);
+    MaxAPI.post(...this.stringify(args), MaxAPI.POST_LEVELS.INFO);
   }
   error(...args: any[]) {
-    console.error(args);
-    MaxAPI.post(this.stringify(args), MaxAPI.POST_LEVELS.ERROR);
+    console.error(...args);
+    MaxAPI.post(...this.stringify(args), MaxAPI.POST_LEVELS.ERROR);
   }
   warn(...args: any[]) {
-    console.warn(args);
-    MaxAPI.post(this.stringify(args), MaxAPI.POST_LEVELS.WARN);
+    console.warn(...args);
+    MaxAPI.post(...this.stringify(args), MaxAPI.POST_LEVELS.WARN);
   }
 }
 
